d4f1b7da1701dea995871fca3a255e0d4238a267
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-08-21 19:17:11 +0900

    refactor: simplify LazyComponent typing

--- src/components/lazy-component.tsx
+++ src/components/lazy-component.tsx
@@ -1,8 +1,8 @@
 import { usePromise } from "../utils/hooks";
 
-export function LazyComponent<P = {}, T = React.ComponentType<P>>(props: {
+export function LazyComponent<T>(props: {
   importer: () => Promise<T>;
-  render: (Component: T) => React.ReactNode;
+  render: (data: T) => React.ReactNode;
   fallback?: React.ReactNode;
 }) {
   const query = usePromise(props.importer);
